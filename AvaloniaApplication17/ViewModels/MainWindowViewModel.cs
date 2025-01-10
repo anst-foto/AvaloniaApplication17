ec395bc30fcc359f1433d059aeac3eb846e22525
Starinin Andrey <starinin-andrey@ya.ru>
2025-01-10 20:11:06 +0300

    Update

--- AvaloniaApplication17/ViewModels/MainWindowViewModel.cs
+++ AvaloniaApplication17/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,27 @@
-﻿namespace AvaloniaApplication17.ViewModels;
+﻿using System;
+using System.Collections.ObjectModel;
+using AvaloniaApplication17.Models;
+using ReactiveUI.Fody.Helpers;
+
+namespace AvaloniaApplication17.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    public ObservableCollection<Photo> Photos { get; set; } = [];
+    [Reactive] public Photo? SelectedPhoto { get; set; }
+
+    public MainWindowViewModel()
+    {
+        Photos.Add(new Photo
+        {
+            Uri = new Uri("avares://AvaloniaApplication17/Assets/123.jpg"),
+            Description = "This is a photo"
+        });
+        
+        Photos.Add(new Photo
+        {
+            Uri = new Uri("avares://AvaloniaApplication17/Assets/123.jpg"),
+            Description = "This is a photo"
+        });
+    }
 }
